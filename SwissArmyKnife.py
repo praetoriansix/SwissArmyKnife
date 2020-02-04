f172f79d4d4ee0597ceb8b45899ac9c7555c042c
praetoriansix <36860278+praetoriansix@users.noreply.github.com>
2020-02-04 15:03:52 -0500

    Add files via upload

---
+++ SwissArmyKnife.py
@@ -0,0 +1,10 @@
+# This is a collection of functions and classes useful for CTF and programming competitions
+
+#This is a function to check syntax and debugging
+def printer_fn(string_to_print):
+    print(string_to_print)
+    return string_to_print
+
+g = printer_fn("hello, world")
+print(g)
+
